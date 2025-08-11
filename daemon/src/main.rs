ab91e402c448c1f3ab73314b3d881fcc84106656
MATSUSHITA Isato <bean@beanlog.xyz>
2025-08-11 15:29:54 +0900

    Day2

--- daemon/src/main.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use daemon::run_daemon;
-
-fn main() -> Result<()> {
-    // Start the background daemon (HTTP server over UDS)
-    println!("Starting Proxy Switcher daemon...");
-    run_daemon()
-}
